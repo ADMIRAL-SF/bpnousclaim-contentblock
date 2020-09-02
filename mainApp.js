f25ce93471028cbff716cab76ae0fba7abe6e0c4
sh-pers <sandra_huett@persistent.com>
2020-09-02 15:43:44 +0200

    d

--- mainApp.js
+++ mainApp.js
@@ -4,7 +4,7 @@
 
 let sdk = new window.sfdc.BlockSDK(); //initalize SDK
 
-let mapData = {
+let blockData = {
     amount: ''
 };
 
@@ -13,161 +13,113 @@ let defaultContent = '<p style="padding-top:10px;background-color:#ff0000;color:
 let saveData = () => {
     console.log('Saving data...');
 
-    mapData.amount = document.getElementById('amount').value;
-    mapData.ticket_type = document.getElementById('ticket_type').value;
-    mapData.external_promotion = document.getElementById('external_promotion').value;
-    mapData.external_promotion_id = document.getElementById('external_promotion_id').value;
-    mapData.issuance_period = document.getElementById('issuance_period').value;
-    mapData.redemption_period = document.getElementById('redemption_period').value;
-    mapData.ticket_text_part_1 = document.getElementById('ticket_text_part_1').value;
-    mapData.ticket_text_part_2 = document.getElementById('ticket_text_part_2').value;
+    blockData.amount = document.getElementById('amount').value;
+    blockData.ticket_type = document.getElementById('ticket_type').value;
+    blockData.external_promotion = document.getElementById('external_promotion').value;
+    blockData.external_promotion_id = document.getElementById('external_promotion_id').value;
+    blockData.issuance_period = document.getElementById('issuance_period').value;
+    blockData.redemption_period = document.getElementById('redemption_period').value;
+    blockData.ticket_text_part_1 = document.getElementById('ticket_text_part_1').value;
+    blockData.ticket_text_part_2 = document.getElementById('ticket_text_part_2').value;
 
-    mapData.issuance = document.getElementById('issuance_schedule').checked;
-    mapData.monday_i = document.getElementById('monday_i').checked;
-    mapData.tuesday_i = document.getElementById('tuesday_i').checked;
-    mapData.wednesday_i = document.getElementById('wednesday_i').checked;
-    mapData.thursday_i = document.getElementById('thursday_i').checked;
-    mapData.friday_i = document.getElementById('friday_i').checked;
-    mapData.saturday_i = document.getElementById('saturday_i').checked;
-    mapData.sunday_i = document.getElementById('sunday_i').checked;
-    mapData.timefrom_i = document.getElementById('timefrom_i').value;
-    mapData.timeto_i = document.getElementById('timeto_i').value;
+    blockData.issuance = document.getElementById('issuance_schedule').checked;
+    blockData.monday_i = document.getElementById('monday_i').checked;
+    blockData.tuesday_i = document.getElementById('tuesday_i').checked;
+    blockData.wednesday_i = document.getElementById('wednesday_i').checked;
+    blockData.thursday_i = document.getElementById('thursday_i').checked;
+    blockData.friday_i = document.getElementById('friday_i').checked;
+    blockData.saturday_i = document.getElementById('saturday_i').checked;
+    blockData.sunday_i = document.getElementById('sunday_i').checked;
+    blockData.timefrom_i = document.getElementById('timefrom_i').value;
+    blockData.timeto_i = document.getElementById('timeto_i').value;
 
-    mapData.redemption = document.getElementById('redemption_schedule').checked;
-    mapData.monday_r = document.getElementById('monday_r').checked;
-    mapData.tuesday_r = document.getElementById('tuesday_r').checked;
-    mapData.wednesday_r = document.getElementById('wednesday_r').checked;
-    mapData.thursday_r = document.getElementById('thursday_r').checked;
-    mapData.friday_r = document.getElementById('friday_r').checked;
-    mapData.saturday_r = document.getElementById('saturday_r').checked;
-    mapData.sunday_r = document.getElementById('sunday_r').checked;
-    mapData.timefrom_r = document.getElementById('timefrom_r').value;
-    mapData.timeto_r = document.getElementById('timeto_r').value;
+    blockData.redemption = document.getElementById('redemption_schedule').checked;
+    blockData.monday_r = document.getElementById('monday_r').checked;
+    blockData.tuesday_r = document.getElementById('tuesday_r').checked;
+    blockData.wednesday_r = document.getElementById('wednesday_r').checked;
+    blockData.thursday_r = document.getElementById('thursday_r').checked;
+    blockData.friday_r = document.getElementById('friday_r').checked;
+    blockData.saturday_r = document.getElementById('saturday_r').checked;
+    blockData.sunday_r = document.getElementById('sunday_r').checked;
+    blockData.timefrom_r = document.getElementById('timefrom_r').value;
+    blockData.timeto_r = document.getElementById('timeto_r').value;
 
-    mapData.availability = document.getElementById('availability').checked;
-    mapData.regions = document.getElementById('regions').value;
-    mapData.locations = document.getElementById('locations').value;
+    blockData.availability = document.getElementById('availability').checked;
+    blockData.regions = document.getElementById('regions').value;
+    blockData.locations = document.getElementById('locations').value;
 
-    sdk.setData(mapData, (data) => {
-        // mapData = data;
+    sdk.setData(blockData, (data) => {
+        // blockData = data;
         var content = '%%[ ';
-        content += 'set @amount = "' + mapData.amount + '" ';
-        content += 'set @ticket_type = "' + mapData.ticket_type + '" ';
-        content += 'set @external_promotion = "' + mapData.external_promotion + '" ';
-        content += 'set @external_promotion_id = "' + mapData.external_promotion_id + '" ';
-        content += 'set @issuance_period = "' + mapData.issuance_period + '" ';
-        content += 'set @redemption_period = "' + mapData.redemption_period + '" ';
-        content += 'set @ticket_text_part_1 = "' + mapData.ticket_text_part_1 + '" ';
-        content += 'set @ticket_text_part_2 = "' + mapData.ticket_text_part_2 + '" ';
+        content += 'set @amount = "' + blockData.amount + '" ';
+        content += 'set @ticket_type = "' + blockData.ticket_type + '" ';
+        content += 'set @external_promotion = "' + blockData.external_promotion + '" ';
+        content += 'set @external_promotion_id = "' + blockData.external_promotion_id + '" ';
+        content += 'set @issuance_period = "' + blockData.issuance_period + '" ';
+        content += 'set @redemption_period = "' + blockData.redemption_period + '" ';
+        content += 'set @ticket_text_part_1 = "' + blockData.ticket_text_part_1 + '" ';
+        content += 'set @ticket_text_part_2 = "' + blockData.ticket_text_part_2 + '" ';
 
-        if (mapData.issuance == true) {
-            content += 'set @timefrom_i = "' + mapData.timefrom_i + '" ';
-            content += 'set @timeto_i = "' + mapData.timeto_i + '" ';
+        if (blockData.issuance == true) {
+            content += 'set @timefrom_i = "' + blockData.timefrom_i + '" ';
+            content += 'set @timeto_i = "' + blockData.timeto_i + '" ';
 
-            if (mapData.monday_i == true) {
+            if (blockData.monday_i == true) {
                 content += 'set @monday_i = "true" ';
             }
-            if (mapData.tuesday_i == true) {
+            if (blockData.tuesday_i == true) {
                 content += 'set @tuesday_i = "true" ';
             }
-            if (mapData.wednesday_i == true) {
+            if (blockData.wednesday_i == true) {
                 content += 'set @wednesday_i = "true" ';
             }
-            if (mapData.thursday_i == true) {
+            if (blockData.thursday_i == true) {
                 content += 'set @thursday_i = "true" ';
             }
-            if (mapData.friday_i == true) {
+            if (blockData.friday_i == true) {
                 content += 'set @friday_i = "true" ';
             }
-            if (mapData.saturday_i == true) {
+            if (blockData.saturday_i == true) {
                 content += 'set @saturday_i = "true" ';
             }
-            if (mapData.sunday_i == true) {
+            if (blockData.sunday_i == true) {
                 content += 'set @sunday_i = "true" ';
             }
         }
 
-        if (mapData.redemption == true) {
-            content += 'set @timefrom_r = "' + mapData.timefrom_r + '" ';
-            content += 'set @timeto_r = "' + mapData.timeto_r + '" ';
+        if (blockData.redemption == true) {
+            content += 'set @timefrom_r = "' + blockData.timefrom_r + '" ';
+            content += 'set @timeto_r = "' + blockData.timeto_r + '" ';
             
-            if (mapData.monday_r == true) {
+            if (blockData.monday_r == true) {
                 content += 'set @monday_r = "true" ';
             }
-            if (mapData.tuesday_r == true) {
+            if (blockData.tuesday_r == true) {
                 content += 'set @tuesday_r = "true" ';
             }
-            if (mapData.wednesday_r == true) {
+            if (blockData.wednesday_r == true) {
                 content += 'set @wednesday_r = "true" ';
             }
-            if (mapData.thursday_r == true) {
+            if (blockData.thursday_r == true) {
                 content += 'set @thursday_r = "true" ';
             }
-            if (mapData.friday_r == true) {
+            if (blockData.friday_r == true) {
                 content += 'set @friday_r = "true" ';
             }
-            if (mapData.saturday_r == true) {
+            if (blockData.saturday_r == true) {
                 content += 'set @saturday_r = "true" ';
             }
-            if (mapData.sunday_r == true) {
+            if (blockData.sunday_r == true) {
                 content += 'set @sunday_r = "true" ';
             }
         }
 
-        if (mapData.availability == true) {
-            content += 'set @availability_regions = "' + mapData.regions + '" ';
-            content += 'set @availability_locations = "' + mapData.locations + '" ';
+        if (blockData.availability == true) {
+            content += 'set @availability_regions = "' + blockData.regions + '" ';
+            content += 'set @availability_locations = "' + blockData.locations + '" ';
         }
 
-
-
         content += ']%%';
-
-        /*`%%[set @Description = 'test' set @Name = 'test2']%%
-<tr>
-  <td align="center" valign="top" >
-    <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%">
-      <tr>
-        <!-- added padding here -->
-        <td class="content_padding" style="padding : 0px 0px 0px 0px; ">
-          <!-- end of comment -->
-          <table border="0" cellpadding="0" cellspacing="0" width="100%">
-            <tr> <!-- top slot -->
-              <td align="center" class="header" valign="top">
-                <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%">
-                  <tbody>
-                      <tr>
-                        <td align="left" valign="top">
-                          <table cellspacing="0" cellpadding="0" style="width:100%">
-                            <tbody>
-                            <tr>
-                              <td class="responsive-td" valign="top" style="width: 100%;">
-                                <table cellpadding="0" cellspacing="0" width="100%" style="background-color: transparent; min-width: 100%; " class="slot-styling"><tr><td style="padding: 0px 15px; " class="slot-styling camarker-inner"><table cellpadding="0" cellspacing="0" width="100%" style="min-width: 100%; " class="stylingblock-content-wrapper"><tr><td style="padding: 10px; " class="stylingblock-content-wrapper camarker-inner"><table cellspacing="0" cellpadding="0" style="width: 100%;"><tr><td><table cellspacing="0" cellpadding="0" style="width: 100%;"><tr><td valign="top" class="responsive-td" style="width: 50%; padding-right: 5px;"><table cellpadding="0" cellspacing="0" width="100%" style="min-width: 100%; " class="stylingblock-content-wrapper"><tr><td class="stylingblock-content-wrapper camarker-inner"><table width="100%" cellspacing="0" cellpadding="0"><tr><td align="center"><img src="%%=v(@Image)=%%" width="850" style="display: block; padding: 0px; text-align: center; border: 0px solid transparent; height: auto; width: 100%;"></td></tr></table></td></tr></table></td><td valign="top" class="responsive-td" style="width: 50%; padding-left: 5px;"><table cellpadding="0" cellspacing="0" width="100%" style="min-width: 100%; " class="stylingblock-content-wrapper"><tr><td class="stylingblock-content-wrapper camarker-inner"><b>%%=v(@Name)=%%</b><br>
-<i>%%=v(@Description)=%% </i><br>
-<br>
-<br>
-<span style="font-size:18px;"><b>%%=v(@Price)=%% €</b></span><br>
-&nbsp;</td></tr></table><table cellpadding="0" cellspacing="0" width="100%" style="background-color: transparent; min-width: 100%; " class="stylingblock-content-wrapper"><tr><td style="padding: 0px 0px 20px; " class="stylingblock-content-wrapper camarker-inner"><table align="left" bgcolor="#00a1f4" border="0" cellpadding="0" cellspacing="0">
- 
-  <tr>
-   <td style="font-family: Arial; font-size: 16px; line-height: 19px; color: #ffffff; text-align:center; font-weight:normal; ">
-    <a alias="" conversion="false" data-linkto="other" href="%%=v(@Link)=%%" style="color:#ffffff;text-decoration:none;font-family: Arial; font-size: 16px; line-height: 19px; text-align:center; font-weight:normal; background-color: #00a1f4;border: 10px solid #00a1f4;display: block; white-space: nowrap; " target="_blank" title="">Get them now</a></td></tr></table></td></tr></table></td></tr></table></td></tr></table></td></tr></table></td></tr></table>
-                              </td>
-                            </tr>
-                            </tbody>
-                          </table>
-                        </td>
-                      </tr>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
-          </table>
-        </td>
-      </tr>
-    </table>
-  </td>
-</tr>`*/
         let superContent = defaultContent;
 
 
@@ -183,7 +135,7 @@ let saveData = () => {
 
     });
 
-    console.log(JSON.stringify(mapData));
+    console.log(JSON.stringify(blockData));
 
 }
 
@@ -193,48 +145,48 @@ let fetchData = () => {
 
     sdk.getData((data) => {
         if (Object.keys(data).length > 0) {
-            mapData = data;
+            blockData = data;
 
-            document.getElementById('amount').value = mapData.amount;
-            document.getElementById('ticket_type').value = mapData.ticket_type;
-            document.getElementById('external_promotion').value = mapData.external_promotion;
-            document.getElementById('external_promotion_id').value = mapData.external_promotion_id;
-            document.getElementById('issuance_period').value = mapData.issuance_period;
-            document.getElementById('redemption_period').value = mapData.redemption_period;
-            document.getElementById('ticket_text_part_1').value = mapData.ticket_text_part_1;
-            document.getElementById('ticket_text_part_2').value = mapData.ticket_text_part_2;
+            document.getElementById('amount').value = blockData.amount;
+            document.getElementById('ticket_type').value = blockData.ticket_type;
+            document.getElementById('external_promotion').value = blockData.external_promotion;
+            document.getElementById('external_promotion_id').value = blockData.external_promotion_id;
+            document.getElementById('issuance_period').value = blockData.issuance_period;
+            document.getElementById('redemption_period').value = blockData.redemption_period;
+            document.getElementById('ticket_text_part_1').value = blockData.ticket_text_part_1;
+            document.getElementById('ticket_text_part_2').value = blockData.ticket_text_part_2;
 
-            document.getElementById('issuance_schedule').checked = mapData.issuance;
-            document.getElementById('monday_i').checked = mapData.monday_i;
-            document.getElementById('tuesday_i').checked = mapData.tuesday_i;
-            document.getElementById('wednesday_i').checked = mapData.wednesday_i;
-            document.getElementById('thursday_i').checked = mapData.thursday_i;
-            document.getElementById('friday_i').checked = mapData.friday_i;
-            document.getElementById('saturday_i').checked = mapData.saturday_i;
-            document.getElementById('sunday_i').checked = mapData.sunday_i;
-            document.getElementById('timefrom_i').value = mapData.timefrom_i;
-            document.getElementById('timeto_i').value = mapData.timeto_i;
+            document.getElementById('issuance_schedule').checked = blockData.issuance;
+            document.getElementById('monday_i').checked = blockData.monday_i;
+            document.getElementById('tuesday_i').checked = blockData.tuesday_i;
+            document.getElementById('wednesday_i').checked = blockData.wednesday_i;
+            document.getElementById('thursday_i').checked = blockData.thursday_i;
+            document.getElementById('friday_i').checked = blockData.friday_i;
+            document.getElementById('saturday_i').checked = blockData.saturday_i;
+            document.getElementById('sunday_i').checked = blockData.sunday_i;
+            document.getElementById('timefrom_i').value = blockData.timefrom_i;
+            document.getElementById('timeto_i').value = blockData.timeto_i;
 
-            document.getElementById('redemption_schedule').checked = mapData.redemption;
-            document.getElementById('monday_r').checked = mapData.monday_r;
-            document.getElementById('tuesday_r').checked = mapData.tuesday_r;
-            document.getElementById('wednesday_r').checked = mapData.wednesday_r;
-            document.getElementById('thursday_r').checked = mapData.thursday_r;
-            document.getElementById('friday_r').checked = mapData.friday_r;
-            document.getElementById('saturday_r').checked = mapData.saturday_r;
-            document.getElementById('sunday_r').checked = mapData.sunday_r;
-            document.getElementById('timefrom_r').value = mapData.timefrom_r;
-            document.getElementById('timeto_r').value = mapData.timeto_r;
+            document.getElementById('redemption_schedule').checked = blockData.redemption;
+            document.getElementById('monday_r').checked = blockData.monday_r;
+            document.getElementById('tuesday_r').checked = blockData.tuesday_r;
+            document.getElementById('wednesday_r').checked = blockData.wednesday_r;
+            document.getElementById('thursday_r').checked = blockData.thursday_r;
+            document.getElementById('friday_r').checked = blockData.friday_r;
+            document.getElementById('saturday_r').checked = blockData.saturday_r;
+            document.getElementById('sunday_r').checked = blockData.sunday_r;
+            document.getElementById('timefrom_r').value = blockData.timefrom_r;
+            document.getElementById('timeto_r').value = blockData.timeto_r;
 
-            document.getElementById('availability').checked = mapData.availability;
-            document.getElementById('regions').value = mapData.regions;
-            document.getElementById('locations').value = mapData.locations;
+            document.getElementById('availability').checked = blockData.availability;
+            document.getElementById('regions').value = blockData.regions;
+            document.getElementById('locations').value = blockData.locations;
 
             console.log('Found data!');
         }
     });
 
-    console.log(JSON.stringify(mapData));
+    console.log(JSON.stringify(blockData));
 }
 
 window.onload = fetchData;
